6d0e7e2f3023b95f6fcfa7f27159199d06131251
sidssn <48656789+sidssn@users.noreply.github.com>
2022-02-23 15:15:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, session
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World! This is done by azure pipelines"
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return "Logout success!"
